3c9c01dac59ce36eb37cfb0bc5de6315782bc00f
gulmira2018 <yestekovagulmir@gmail.com>
2018-11-21 16:59:07 -0500

    hi

--- 1-My First program/src/Main.java
+++ 1-My First program/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 	
 	public static void main(String[] arg){
 	
-//	System.out.println("Hello world");
-//    System.out.println("today is first day of java");
+	System.out.println("Hello world");
+    System.out.println("today is first day of java");
 //	
 //	System.out.println("My name is Mike");
 //	
